4d1ab5de6ad728a67a1a7a630720791d02980f14
Tyler Hughes <Tyler.J.Hug@gmail.com>
2026-04-20 22:24:21 -0400

    test commit

---
+++ Project1/main.py
@@ -0,0 +1,10 @@
+# This is a name generator. Started at 10:20 PM on April 20th, 2026.
+import random
+
+first_names = ["Jefferson", "Connor", "Ebert", "Spoot", "Ranky"]
+last_names = ["Diaper", "Humperdink", "Patrick", "Sniffer", "Gnauh"]
+
+x = random.choice(first_names)
+y = random.choice(last_names)
+
+print(x, y)
